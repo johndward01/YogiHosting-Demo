205398e35fc5c4697badda474d46150cb28e7371
jdw <johndward01@gmail.com>
2021-10-01 16:26:11 -0500

    Add project files.

---
+++ YogiHosting Demo App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace YogiHosting_Demo_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
